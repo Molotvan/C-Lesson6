06691f6a5436066b6a5a624db7c07603c5c86625
Molotvan <molotvan@yandex.ru>
2022-09-11 23:21:46 +0300

    init

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Привет. Введи по очереди несколько целых чисел(можно отрицательные). Когда надоест, введи слово " + "конец");
+
+CountNegative(0);
+
+void CountNegative(int count)
+{
+    string input = Console.ReadLine();
+    if (input.ToLower() != "конец")
+    {
+        if (Int32.Parse(input) < 0)
+        {
+            count++;
+        }
+        CountNegative(count);
+    }
+    else
+    {
+        Console.WriteLine("вы ввели " + count + " отрицательных чисел");
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿double k1 = Int32.Parse(Console.ReadLine());
+double k2 = Int32.Parse(Console.ReadLine());
+double b1 = Int32.Parse(Console.ReadLine()); 
+double b2 = Int32.Parse(Console.ReadLine());
+double x = (b2-b1)/(k1-k2);
+double y = k1*x + b1;
+Console.WriteLine("Координаты общих точек: x = " + x + ", y = " + y );
